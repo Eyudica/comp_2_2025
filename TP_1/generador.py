c0ce077ca1ffcf099091cab3f173c2ebba70e9b3
Eyudica <e.yudica@alumno.um.edu.ar>
2025-06-19 21:59:29 -0300

    Upload documented tp1 files

--- TP_1/generador.py
+++ TP_1/generador.py
@@ -11,7 +11,7 @@ from hashlib import sha256
 import sys
 import signal
 PAQUETES = 60
-
+#Genera una muestra aleatoria
 def generar_muestra():
     return {
         "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -19,7 +19,7 @@ def generar_muestra():
         "presion": random.randint(110,180),# random.randint(70,110)],
         "oxigeno": random.randint(90,100)
     }
-
+#Calcula la media y desviacion de cada tipo de dato, enviando los resultados al queue para el proceso verificador
 def make_math(conn,clave,queue):
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
@@ -48,10 +48,9 @@ def make_math(conn,clave,queue):
         except EOFError:
             break    
 
-       # if clave=="presion":
-
         
-
+#Genera las 60 muestras y las envia al pipe que esta conectado cada proceso que haga los calculos
+#de la media y de la desviacion (make_math)
 def make_muestra(conns):
     signal.signal(signal.SIGINT, signal.SIG_IGN)
     print("Generando muestras.Espera 60")
@@ -60,7 +59,7 @@ def make_muestra(conns):
         for conn in conns:
             conn.send(muestra)
         time.sleep(1)
-
+#Tiene un queue de entrada para recibir las muestras y un queue de salida para enviar los bloques analizados
 def validar(queue_entrada,queue_salida):
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
@@ -68,33 +67,34 @@ def validar(queue_entrada,queue_salida):
     bloques_por_timestamp = {}
     prev_hash = "00000000000000000000000000000000000000000000000000000000000000000"
     bloques_procesados = 0
-
+    #Esto se va a ejecutar en este caso 60 veces
     while bloques_procesados < PAQUETES:
         resultado = queue_entrada.get()
         ts = resultado["timestamp"]
-
+        #Por timestamp guarda los datos en el diccionario
+        #si el timestamp es nuevo crea una nueva entrada en el diccioniario
         if ts not in bloques_por_timestamp:
             bloques_por_timestamp[ts] = {}
         bloques_por_timestamp[ts][resultado["tipo"]] = resultado
-
+        #Si estan los 3 tipos de datos arma el bloque
         if len(bloques_por_timestamp[ts]) == 3:
             datos = bloques_por_timestamp[ts]
-
+            #Calcula si hay alerta o no
             alerta = (
                 datos["frecuencia"]["media"] > 200 or
                 (90>=datos["oxigeno"]["media"] >=100 )or
                 datos["presion"]["media"] > 200
             )
-
+            #Arma los datos en formato json
             datos_str = json.dumps({
                 "frecuencia": datos["frecuencia"],
                 "presion": datos["presion"],
                 "oxigeno": datos["oxigeno"]
             }, sort_keys=True)
-
+            #Calcula el hash de la cadena
             bloque_raw = prev_hash + datos_str + ts
             current_hash = sha256(bloque_raw.encode("utf-8")).hexdigest()
-
+            #Arma el bloque para meterlo en la lista que despues se va a enviar al queue de salida
             bloque = {
                 "timestamp": ts,
                 "datos": datos,
@@ -107,10 +107,8 @@ def validar(queue_entrada,queue_salida):
             bloques_procesados += 1
             bloques.append(bloque)
     queue_salida.put(bloques)
-    #print(f"Bloques procesados: {bloques}")
-            #guardar_bloque(bloque)
 
-            #print(f"Bloque {bloques_procesados} guardado. Alerta: {alerta}")
+#Sirve para escribir los bloques en el archivo .json y que si existe no lo sobreescriba
 def guardar_bloque(bloque, archivo="blockchain.json"):
     if os.path.exists(archivo):
         with open(archivo, "r") as f:
@@ -123,7 +121,7 @@ def guardar_bloque(bloque, archivo="blockchain.json"):
     with open(archivo, "w") as f:
         json.dump(cadena, f, indent=4)
 
-
+#Inicia los procesos y sus respectivos pipes
 def iniciar_procesos(tipo,queue):
     parent_conn, child_conn = multiprocessing.Pipe()
     send = multiprocessing.Process(target=make_muestra, args=(parent_conn,))
@@ -132,38 +130,43 @@ def iniciar_procesos(tipo,queue):
     receptor = multiprocessing.Process(target=make_math, args=(child_conn,tipo,queue))
     receptor.start()
     receptor.join()
-
+#Para atrapar el ctrl+c
 def handler(sig,frame):
     print("\nEl usuario presiono la tecla CTRL+C")
     os._exit(1)
 
 def main():
+    #para que los hijos no hereden el handler
     if multiprocessing.current_process().name=="MainProcess":
         signal.signal(signal.SIGINT, handler)
-
+    #crealos pipes para  los procesos
     pipes=[]
     for _ in range(3):
         pipes.append(multiprocessing.Pipe())
     queue=multiprocessing.Queue()
     queue_resultados_validados=multiprocessing.Queue()
+    #Genera las muestras y los envia a cada pipe
     generador_p=multiprocessing.Process(target=make_muestra, args=([p[0] for p in pipes],))
     generador_p.start()
     
     tipos=["frecuencia", "presion", "oxigeno"]
     analizadores=[]
+    #Lanza los 3 procesos y  le pasa el pipe hijo al  proceso hijo
     for i,tipo in enumerate(tipos):
         p=multiprocessing.Process(target=make_math, args=(pipes[i][1],tipo,queue))
         p.start()
-        analizadores.append(p)
+        analizadores.append(p)#--> se usa analizadores para guardar los procesos  hijos
     verificador_p=multiprocessing.Process(target=validar, args=(queue,queue_resultados_validados))
     verificador_p.start()
     procesos = [generador_p] + analizadores + [verificador_p]
 
     try:
         generador_p.join()
+        #espera a que terminen los hijos
         for p in analizadores:
             p.join()
         verificador_p.join()
+    #para que si el usuario hace  ctrl+c corte todo y que no queden hijos huerfanos
     except KeyboardInterrupt:
         print("Ctrl+C detectado. Terminando todos los procesos hijos...")
         for p in procesos:
@@ -172,8 +175,9 @@ def main():
         for p in procesos:
             p.join()
         sys.exit(1)
-
+    #Obtiene los bloques del queue de salida
     bloques=queue_resultados_validados.get()
+    #Cierra los  queues
     queue.close()
     queue_resultados_validados.close()
 
--- TP_1/verificar_cadena.py
+++ TP_1/verificar_cadena.py
@@ -1,13 +1,18 @@
 from hashlib import sha256
 import json
-
+#Abre el archivo .json
 def open_file():
     with open("blockchain.json") as f:
        
         return json.load(f)
+def check_total_blocks():
+    with open("blockchain.json") as f:
+        data = json.load(f)
+        return len(data)
+#Recalcula el hash y se fija que sean iguales que el del archivo json
 def validar(data):
-    #prev_hash="00000000000000000000000000000000000000000000000000000000000000000"
     cadenas_corruptas=0
+    hashes_corruptos=[]
     for posicion,lista_blockchain in enumerate(data):
         current_hash=lista_blockchain['hash']
         prev_hash = lista_blockchain['prev_hash']
@@ -20,9 +25,9 @@ def validar(data):
         hash_recalculado = sha256(bloque_raw.encode("utf-8")).hexdigest()
         if current_hash!=hash_recalculado:
             cadenas_corruptas+=1
-            print(f"Cadena corrupta: {current_hash} no es igual a {hash_recalculado} en el bloque {posicion+1}")
-    print("Validacion finalizada")
-    return cadenas_corruptas
+            hashes_corruptos.append(f"Cadena corrupta: {current_hash} no es igual a {hash_recalculado} en el bloque {posicion+1}")
+    return hashes_corruptos
+#Calcula la media y desviacion total de cada tipo
 def calcular_totales(cadena,tipo):
     media_valores=[]
     desviacion_valores=[]
@@ -38,11 +43,16 @@ if __name__ == "__main__":
     
     cadena = open_file()
     corruptos=validar(cadena)
+    len_data=check_total_blocks()
+    #Arma el archivo .txt y muestra que bloques estan corruptos si es que hubieron
     with open("resultados.txt","w") as f:
-        if corruptos==0:
-            f.write("No hubo bloques corruptos de 60 analizados\n")
+        if len(corruptos)==0:
+            f.write(f"No hubo bloques corruptos de {len_data} analizados\n")
         else:
-            f.write(f"Hubieron un total de {corruptos} bloques corruptos de 60 analizados\n")
+            f.write(f"Hubieron un total de {len(corruptos)} bloques corruptos de {len_data} analizados\n")
+            for cadena_corrupta in corruptos:
+                f.write(cadena_corrupta+"\n")
+            
         for tipo in ["frecuencia","presion","oxigeno"]:
             valores_finales=calcular_totales(cadena,tipo)
             f.write("-"*40+"\n")
